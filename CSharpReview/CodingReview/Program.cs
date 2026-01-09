273a3b44d89890c9c2f823d06f726cbfd27096d2
jordan711 <jordan.feng@hotmail.com>
2026-01-08 23:46:53 -0500

    New C sharp console project

---
+++ CSharpReview/CodingReview/Program.cs
@@ -0,0 +1,11 @@
+﻿// dotnet new console -n <name> 
+// Above command creates a new c# project
+
+// dotnet new list
+// Templates of projects to make
+
+// dotnet run
+// Run the project
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
