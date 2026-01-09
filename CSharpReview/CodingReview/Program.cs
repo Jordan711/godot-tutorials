9562c152d078aeef646b93d462fa1c7acff9bfce
jordan711 <jordan.feng@hotmail.com>
2026-01-09 01:24:19 -0500

    C Sharp Basics Review

--- CSharpReview/CodingReview/Program.cs
+++ CSharpReview/CodingReview/Program.cs
@@ -1,4 +1,6 @@
-﻿// dotnet new console -n <name> 
+﻿using System.Collections.Generic;
+
+// dotnet new console -n <name> 
 // Above command creates a new c# project
 
 // dotnet new list
@@ -9,3 +11,107 @@
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Variables
+int age = 22;
+string name = "Frodo";
+bool isHero = false;
+double height = 1.4567; // 16 bit 16dp
+float weight = 25.6f; // 32 bit 7dp
+char allegiance = 'G';
+
+const int SPEED = 22;
+
+Console.WriteLine($"{name} is \n {age} years old");
+
+var isVillian = true;
+
+float floatValue = 50.456f;
+int intValue = 23434;
+
+float division = intValue / floatValue; // Yields a float value
+Console.WriteLine(division);
+
+
+// If Statements
+if (isVillian == false)
+{
+    Console.WriteLine("Is Villian");
+}
+else
+{
+    Console.WriteLine("Is Not Villian");
+}
+
+// Switch statements
+switch(name)
+{
+    case "Bob":
+        Console.WriteLine("The name is Bob");
+        break;
+    case "Sarah":
+    case "Billy":
+    case "John":
+    case "Donkey":
+        Console.WriteLine($"The name is {name}");
+        break;
+    default:
+        Console.WriteLine("Unknown name is intruder");
+        break;
+}
+
+int x = 10;
+string result = x > 10 ? "LARGE" : "SMALL";
+
+// Inputs
+Console.Write("Enter your name: ");
+string userName = Console.ReadLine();
+
+Console.Write("Age: ");
+int inputAge = int.Parse(Console.ReadLine());
+
+
+// Arrays and loops
+string[] clans = {"Fire", "Water", "Earth", "Air"};
+
+Console.WriteLine(clans[2]);
+Console.WriteLine(clans.Length);
+clans [2] = "Blue";
+Console.WriteLine(clans[2]);
+
+for (int i = 0; i < clans.Length; i++)
+{
+    Console.WriteLine(clans[i]);
+}
+
+foreach (var item in clans)
+{
+    if (item == "Blue")
+    {
+        Console.WriteLine("Woohoo");
+        continue;
+    }
+    Console.WriteLine(item);
+}
+
+// List<string> inventory = new List<string>();
+
+// inventory.Add("Box");
+// inventory.Add("Food");
+// inventory.Add("Money");
+
+List<string> inventory = new List<string>(
+    "Pipe", "Glamdring", "Wizard Staff"
+);
+
+inventory.Remove("Money");
+
+foreach (var item in inventory)
+{
+    Console.WriteLine(item);
+}
+Console.WriteLine(inventory.Contains("Money"));
+
+List<string> sortedInventory = inventory.Order().ToList(); // Sorted in alphabetical order
+
+string desiredItem = inventory.FirstOrDefault(item => item == "Glamdring");
